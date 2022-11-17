46a3999cf8f099c3ce54ec038bcb9951389482df
AdiSchiff <adidi.schiff@gmail.com>
2022-11-17 20:01:03 +0200

    creating the functions of Distance.cpp

--- Distance.cpp
+++ Distance.cpp
@@ -1,1 +1,68 @@
+/*
+Ex 1 part 2
+Idit Medizada 318879293
+Adi Schiff 212730675
+https://github.com/AdiSchiff/Idit-Adi.git
+*/
 
+# include <vector>
+
+/******************
+* Function Name: euclideanDistance
+* Input: 2 vectors
+* Output: the euclidean distance between the vectors
+* Function Operation:calculating the euclidean distance between two vectors
+* acording to: d(p,q)={sqrt {(p-q)^{2}}}
+* for ellaboration: https://en.wikipedia.org/wiki/Euclidean_distance
+* ******************/
+double euclideanDistance() {
+
+}
+
+/******************
+* Function Name: manhattanDistance
+* Input: 2 vectors
+* Output: the manhattan distance between the vectors
+* Function Operation:calculating the euclidean distance between two vectors
+* acording to: d(p,q)=|p-q|=sum_{i=1}^{n} |p_{i} - q_{i}|
+* for ellaboration: https://en.wikipedia.org/wiki/Taxicab_geometry
+* ******************/
+double manhattanDistance() {
+
+}
+
+/******************
+* Function Name: chebyshevDistance
+* Input: 2 vectors
+* Output: the chebyshev distance between the vectors
+* Function Operation:calculating the euclidean distance between two vectors
+* acording to: D(x,y):=max(|x_{i} - y_{i}|)
+* for ellaboration: https://en.wikipedia.org/wiki/Chebyshev_distance
+* ******************/
+double chebyshevDistance() { 
+
+}
+
+/******************
+* Function Name: canberraDistance
+* Input: 2 vectors
+* Output: the canberra distance between the vectors
+* Function Operation:calculating the euclidean distance between two vectors
+* acording to: d(p,q)=sum_{i=1}^{n} {{|p_{i}-q_{i}|} / {|p_{i}|+|q_{i}|}}
+* for ellaboration: https://en.wikipedia.org/wiki/Canberra_distance
+* ******************/
+double canberraDistance() {
+    
+}
+
+/******************
+* Function Name: minkowskiDistance
+* Input: 2 vectors
+* Output: the minkowski distance between the vectors
+* Function Operation:calculating the euclidean distance between two vectors
+* acording to: D(X,Y)=(sum_{i=1}^{n} |x_{i} - y_{i}|^{p})^{\1/p}
+* for ellaboration: https://en.wikipedia.org/wiki/Minkowski_distance
+* ******************/
+double minkowskiDistance() {
+
+}
