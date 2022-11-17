aa7a90ff65080c65fea9c4b4ac8c0e4ae55de17c
AdiSchiff <adidi.schiff@gmail.com>
2022-11-17 19:05:50 +0200

    added file Distance.h

---
+++ Distance.h
@@ -0,0 +1,13 @@
+/*
+Ex 1 part 2
+Idit Medizada 318879293
+Adi Schiff 212730675
+https://github.com/AdiSchiff/Idit-Adi.git
+*/
+
+# include <vector>
+double euclideanDistance;
+double manhattanDistance;
+double chebyshevDistance;
+double canberraDistance;
+double minkowskiDistance;
