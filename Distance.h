b214e711a81b28dcd2773a073baf440ab411bb4f
AdiSchiff <adidi.schiff@gmail.com>
2022-11-17 20:05:45 +0200

    updating Distance.h

--- Distance.h
+++ Distance.h
@@ -5,9 +5,14 @@ Adi Schiff 212730675
 https://github.com/AdiSchiff/Idit-Adi.git
 */
 
+#ifndef VECTORARITHMETIC_H_
+#define VECTORARITHMETIC_H_
 # include <vector>
 double euclideanDistance;
 double manhattanDistance;
 double chebyshevDistance;
 double canberraDistance;
 double minkowskiDistance;
+
+
+#endif
