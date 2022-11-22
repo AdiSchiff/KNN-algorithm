06e045569608289e6380bcf58d2d7cbc31727eac
AdiSchiff <adidi.schiff@gmail.com>
2022-11-22 23:38:49 +0200

    updated Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,17 @@
-/*
-Ex 1 part 2
-Idit Medizada 318879293
-Adi Schiff 212730675
-https://github.com/AdiSchiff/Idit-Adi.git
-*/
-
-#include "VectorArithmetic.h"
-#include "Distance.h"
-#include "ReceiveVector.h"
-#include <vector>
+// #include "VectorArithmetic.h"
+// #include "Distance.h"
+#include "Print.h"
 #include <iostream>
+#include <vector>
+#include "ReceiveVector.h"
 using namespace std;
+
 int main(){
- vector<double> v1 = receiveVector();
- vector<double> v2 = receiveVector();
+vector<double> v1 = receiveVector();
+vector<double> v2 = receiveVector();
   if(v1.size() != v2.size()){//if the vectors don't have the same number of elements
     cout << "ilegal input";
-  exit(0);
-  }
-
-
-
+    exit(0);
+}
+  printAll(v1,v2);
 }
