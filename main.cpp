82c112f11d7c56692e1661accfd63de2fd8f00bf
AdiSchiff <adidi.schiff@gmail.com>
2022-11-22 21:18:39 +0200

    adding the current vertion of main and uploading the json files

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
+/*
+Ex 1 part 2
+Idit Medizada 318879293
+Adi Schiff 212730675
+https://github.com/AdiSchiff/Idit-Adi.git
+*/
+
 #include "VectorArithmetic.h"
 #include "Distance.h"
+#include "ReceiveVector.h"
+#include <vector>
 #include <iostream>
+using namespace std;
 int main(){
- vector<double> num1 {1, 2, 3};
- vector<double> num2 {3, 2, 1};
- double d = minkowskiDistance(num1,num2, 2);
- std:: cout << d;
- /*vector<double> num3= addVectors(num1,num2);
+ vector<double> v1 = receiveVector();
+ vector<double> v2 = receiveVector();
+  if(v1.size() != v2.size()){//if the vectors don't have the same number of elements
+    cout << "ilegal input";
+  exit(0);
+  }
 
-  cout << "Initial Vector: ";
 
-  for (const double& i : num3) {
-   std:: cout << i << "  ";
-  }*/
+
 }
