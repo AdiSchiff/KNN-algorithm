774357ab1a3457de9768b2f65a36840f91626695
AdiSchiff <adidi.schiff@gmail.com>
2023-01-17 16:50:40 +0200

    merging the commands into a single file

--- Command.h
+++ Command.h
@@ -10,15 +10,63 @@ using namespace std;
 
 
 class Command {
-private:
-    string const description;
-    DefaultIO dio;
+protected:
+    string description;
+    DefaultIO * dio;
 public:
+    Command(DefaultIO *_dio);
     string getDescription() const;
-    DefaultIO getDio() const;
     virtual void execute() = 0;
     ~Command();
 };
 
+class Welcome: public Command{
+public:
+    Welcome(DefaultIO *&_dio);
+    void execute() override;
+    ~Welcome();
+};
+
+class UploadData: public Command{
+public:
+    UploadData(DefaultIO *&_dio);
+    void execute() override;
+    ~UploadData();
+};
+
+class AlgoSettings: public Command{
+public:
+    AlgoSettings(DefaultIO *&_dio);
+    void execute() override;
+    ~AlgoSettings();
+};
+
+class Classify: public Command{
+public:
+    Classify(DefaultIO *&_dio);
+    void execute() override;
+    ~Classify();
+};
+
+class Display: public Command{
+public:
+    Display(DefaultIO *&_dio);
+    void execute() override;
+    ~Display();
+};
+
+class Download: public Command{
+public:
+    Download(DefaultIO *&_dio);
+    void execute() override;
+    ~Download();
+};
+
+class Exit: public Command{
+public:
+    Exit(DefaultIO *&_dio);
+    void execute() override;
+    ~Exit();
+};
 
 #endif //CLIENT_COMMA_H
